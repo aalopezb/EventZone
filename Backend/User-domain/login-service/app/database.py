0750ee6fa0bf8a780391ca336c6ee189185d5fdc
aalopezb <a.alexander.l.b@gmail.com>
2025-06-23 17:11:16 -0500

    feat: :bug: test

--- Backend/User-domain/login-service/app/database.py
+++ Backend/User-domain/login-service/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:12345@172.17.0.1 :5432/registerdb")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:12345@172.17.0.1:5432/registerdb")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
